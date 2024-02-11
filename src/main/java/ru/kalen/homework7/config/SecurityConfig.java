bb968a749e3ba6c46b28fbf413a14f8602e35bcf
Vladimir-Kalenskiy <kalenskiy.man@icloud.com>
2024-02-11 21:47:51 +0300

    Finished 7HW

--- src/main/java/ru/kalen/homework7/config/SecurityConfig.java
+++ src/main/java/ru/kalen/homework7/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.kalen.homework7.config;
 
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,21 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/private-data").hasRole("ADMIN")
-//                .antMatchers("/public-data").authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .defaultSuccessUrl("/public-data")
-//                .failureUrl("/login?error")
-//                .and()
-//                .logout()
-//                .logoutSuccessUrl("/")
-//                .and()
-//                .exceptionHandling()
-//                .accessDeniedPage("/access-denied");
+
         http.authorizeRequests()
                 .antMatchers("/private-data").hasRole("ADMIN")
                 .antMatchers("/public-data").authenticated()
@@ -37,8 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .logout()
-                .logoutSuccessUrl("/login")
-                .permitAll();
+                .logoutSuccessUrl("/login");
     }
 
     @Bean
@@ -47,7 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         manager.createUser(User.withDefaultPasswordEncoder()
                 .username("user").password("password").roles("USER").build());
         manager.createUser(User.withDefaultPasswordEncoder()
-                .username("admin").password("password").roles("ADMIN").build());
+                .username("admin").password("password").roles("ADMIN", "USER").build());
         return manager;
     }
+
 }
